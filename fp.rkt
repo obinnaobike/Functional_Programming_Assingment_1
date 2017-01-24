e38a980b4c8692190e8d06976cc90f2c2d9f9c0f
= <obinna.obike@bison.howard.edu>
2017-01-23 23:04:02 -0500

    second commit to problem 2

--- fp.rkt
+++ fp.rkt
@@ -1,4 +1,6 @@
-2. (define (sum-up-numbers-simple L)
-         (if (NULL? L)              0
-         (NOT (LIST? L) 0)
-         (+ (CAR L) (sum-up-numbers-simple (CDR L))) ));
+;2. 
+(define (sum-up-numbers-simple L)
+         ((NULL? L) 0 ;checks to see if the list empty or null
+         (NOT (LIST? L) ) 0 ;checks to see if L is actually list
+         (+ (CAR L) (sum-up-numbers-simple (CDR L))) ))
+;adds the 1st item in the list and adds the remaining.

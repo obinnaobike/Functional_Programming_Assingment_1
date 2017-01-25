f955febf75c0a61ccc0495d2533db2b3fb80d771
= <obinna.obike@bison.howard.edu>
2017-01-24 22:47:57 -0500

    fourth commit used the eval function to see if it worked

--- fp.rkt
+++ fp.rkt
@@ -1,6 +1,8 @@
+
 ;2. 
 (define (sum-up-numbers-simple L)
          (cond
           ((null? L) 0) ;checks to see if list is empty return zero
           ((not (list? L)) (display "not a list")) ; if item is not a list display not a list
-          ((+ (car L) (sum-up-numbers-simple (cdr L))) ))) ; return the first item in the list and add the rest of the numbers
+          (else (eval (cons '+ L))); this is supposed to evaluate the list by adding a + sign so (1 2) becomes (+ 1 2) which evaluates to 3.
+          ;((+ (car L) (sum-up-numbers-simple (cdr L))) ))) ; return the first item in the list and add the rest of the numbers

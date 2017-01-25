2b6cc5d93bab85f677c1b0fdc2c04265bcb0d301
= <obinna.obike@bison.howard.edu>
2017-01-25 17:37:44 -0500

    second commit for the 1st problem

--- fp.rkt
+++ fp.rkt
@@ -3,11 +3,11 @@
   (cond
     ;((null? L) '()) if the list is empty return an empty list
     ((list? L)
-    (append (reverse-general (cdr L)) 
+    (cons (reverse-general (cdr L)) 
             (list (reverse-general (car L)))
             )
-    );
-    ;(not (list? L)) if there is only 1 item in the list return l
+    ); concatenate the very last elements of the list first then the last
+    ;(else (not (list? L))) if there is only 1 item in the list return L
     )
   )
 
@@ -17,3 +17,5 @@
           ((null? L) 0) ;checks to see if list is empty return zero
           ((not (list? L)) L) ; if item is not a list return the item
           ((+ (car L) (sum-up-numbers-simple (cdr L))) ))) ; return the first item in the list and add the rest of the numbers
+
+;4.

2c24ab84e9c9565c889f7f36d470cf91f3261729
= <obinna.obike@bison.howard.edu>
2017-01-24 22:50:50 -0500

    fifth commit removed the eval function and made not a list return the item

--- fp.rkt
+++ fp.rkt
@@ -3,6 +3,5 @@
 (define (sum-up-numbers-simple L)
          (cond
           ((null? L) 0) ;checks to see if list is empty return zero
-          ((not (list? L)) (display "not a list")) ; if item is not a list display not a list
-          (else (eval (cons '+ L))); this is supposed to evaluate the list by adding a + sign so (1 2) becomes (+ 1 2) which evaluates to 3.
-          ;((+ (car L) (sum-up-numbers-simple (cdr L))) ))) ; return the first item in the list and add the rest of the numbers
+          ((not (list? L)) L) ; if item is not a list return the item
+          ((+ (car L) (sum-up-numbers-simple (cdr L))) ))) ; return the first item in the list and add the rest of the numbers

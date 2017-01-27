962f6dd336342ee5b4ce3430e3bb0ff0bb828248
obinnaobike <obinna.obike@gmail.com>
2017-01-27 13:56:49 -0500

    another commit for problem 2

--- fp.rkt
+++ fp.rkt
@@ -17,7 +17,9 @@
          (cond
           ((null? L) 0) ;checks to see if list is empty return zero
           ((not (list? L)) L) ; if item is not a list return the item
-          ((+ (car L) (sum-up-numbers-simple (cdr L))) ))) ; return the first item in the list and add the rest of the number
+          ((not (number? (car L))) (sum-up-numbers-simple (cdr L))) ; if the item is not a number check if the rest of the list is a number
+((+ (car L) (sum-up-numbers-simple (cdr L)))))) ; return the first item in the list and add the rest of the number
+
 ;3
 
 (define (sum-up-numbers-simple L)

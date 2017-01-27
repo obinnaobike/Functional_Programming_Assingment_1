77033b7948dfb0cd4f27eae676a85165a8310372
obinnaobike <obinna.obike@gmail.com>
2017-01-27 00:56:53 -0500

    2nd commit problem 4

--- fp.rkt
+++ fp.rkt
@@ -12,22 +12,30 @@
   )
 
 ;2. 
+
 (define (sum-up-numbers-simple L)
          (cond
           ((null? L) 0) ;checks to see if list is empty return zero
           ((not (list? L)) L) ; if item is not a list return the item
-          ((+ (car L) (sum-up-numbers-simple (cdr L))) ))) ; return the first item in the list and add the rest of the numbe
+          ((+ (car L) (sum-up-numbers-simple (cdr L))) ))) ; return the first item in the list and add the rest of the number
 ;3
+
 (define (sum-up-numbers-simple L)
   (cond 
   ((null? L) '() ; if the list is empty or null return an empty list
       ((list? (car L)) ;if the first item in the list is a list
-          (+ (sum-up-numbers-simple (car L)) (sum-up-numbers-up-simple (cdr li))) 
+          (+ (sum-up-numbers-simple (car L)) (sum-up-numbers-simple (cdr L))) 
 ;call the function recursively on the first item in the list and call it on the rest of the items in the list
-          (+ (car L) (sum-up-numbers-up-simple (cdr L)))))) 
+          (+ (car L) (sum-up-numbers-simple (cdr L)))))) 
 ; otherwise add the first item in the list to the rest of the items in the list
+
 ;4.
+
 (define (min-above-min L1 L2)
+(let 
+minimum-L1 (min(L1))
+(let minimum-L2 (min(L2)))
 	(cond
 	((null? L1) #f)
+	((null? L2) minimum-L1)
 	
